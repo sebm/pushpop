8f06fb30cc140966d37888de2706cc0ab67d4aed
Sebastian Motraghi <sebastian.m@gmail.com>
2011-06-23 19:52:04 +0100

    pretty basic pushing and popping

--- app.js
+++ app.js
@@ -30,11 +30,34 @@ app.configure('production', function(){
 
 // Routes
 
+var the_stack = new Array();
+
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'Express',
+    stack: the_stack
   });
 });
+
+app.get('/push', function(req, res){
+  the_stack.push(Math.random());
+
+  res.render('index', {
+    title: 'Express',
+    stack: the_stack
+  });
+});
+
+app.get('/pop', function(req, res){
+  var popped = (the_stack.length)? the_stack.pop() : null;
+
+  res.render('pop', {
+    title: 'Express',
+    stack: the_stack,
+    popped: popped
+  });
+});
+
 
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
